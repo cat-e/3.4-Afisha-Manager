34f156be2565414b0938624299f4d90a2212fe8c
Ekaterina Hlebalova <khlebalova.e@yandex.ru>
2020-07-19 17:02:03 +0400

    test file names fixed

---
+++ src/test/java/ru/netology/manager/AfishaManager5PosterTest.java
@@ -0,0 +1,47 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Afisha;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AfishaManager5PosterTest {
+
+    private final AfishaManager manager = new AfishaManager(5);
+    private final Afisha first = new Afisha(1, 1, "first", "link1", "genre1");
+    private Afisha second = new Afisha(2, 2, "second", "link2", "genre2");
+    private Afisha third = new Afisha(3, 3, "third", "link3", "genre3");
+    private Afisha fourth = new Afisha(4, 4, "fourth", "link4", "genre4");
+    private Afisha fifth = new Afisha(5, 5, "fifth", "link5", "genre5");
+    private Afisha sixth = new Afisha(6, 6, "sixth", "link6", "genre6");
+    private Afisha seventh = new Afisha(7, 7, "seventh", "link7", "genre7");
+    private Afisha eighth = new Afisha(8, 8, "eighth", "link8", "genre8");
+    private Afisha ninth = new Afisha(9, 9, "ninth", "link9", "genre9");
+    private Afisha tenth = new Afisha(10, 10, "tenth", "link10", "genre10");
+
+    @BeforeEach
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+    }
+
+    @Test
+    public void shouldShowFivePosters() {
+        Afisha[] actual = manager.getAll();
+        Afisha[] expected = new Afisha[]{fifth, fourth, third, second, first};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+}
